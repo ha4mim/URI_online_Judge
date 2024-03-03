f6ac0fbf4efcc6669ff690fa7505eb31d407f3ea
Sheikh Hamim Mahmud <hamim.mahmud11@gmail.com>
2024-03-04 00:43:15 +0600

    commit for push

---
+++ URI_online_Judge_1001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_online_Judge_1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int result = A + B;
+            Console.WriteLine("X = "+ result);
+
+        }
+    }
+}
