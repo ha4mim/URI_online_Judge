065fd62d38878795586197ee385ccb99165c56c7
Sheikh Hamim Mahmud <hamim.mahmud11@gmail.com>
2024-03-06 00:47:24 +0600

    commit for push

---
+++ URI_online_Judge_1003/URI_online_Judge_1003/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_online_Judge_1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int soma = a + b;
+            Console.WriteLine("SOMA = "+soma);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ URI_online_Judge_1004/URI_online_Judge_1004/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_online_Judge_1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int prod = A * B;
+            Console.WriteLine("PROD = " + prod);
+            Console.ReadKey();
+        }
+    }
+}
