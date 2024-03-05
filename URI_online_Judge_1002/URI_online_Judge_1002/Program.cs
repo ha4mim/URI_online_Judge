f4ba38808496aa74eefb64602a44288a5f6b082f
Sheikh Hamim Mahmud <hamim.mahmud11@gmail.com>
2024-03-06 00:21:17 +0600

    commit for pull

---
+++ URI_online_Judge_1002/URI_online_Judge_1002/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_online_Judge_1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = Convert.ToDouble( Console.ReadLine());
+            double pi = 3.1415;
+            double totatl = pi*r * r;
+            Console.WriteLine(" A={0:F4}",totatl);
+            Console.ReadKey();
+        }
+    }
+}
